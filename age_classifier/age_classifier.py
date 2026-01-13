033fbe258f0d7da199f222c9302b84b051702af6
rohit <rohitghosh.07@gmail.com>
2026-01-13 15:33:06 +0530

    First Python project : Age classifier

---
+++ age_classifier/age_classifier.py
@@ -0,0 +1,9 @@
+age = int(input("please eter your age:" ))
+if age < 13 : 
+    print("child")
+elif age < 18 :
+    print("teenager")
+elif age < 60 :
+    print("adult")
+else :
+    print("senior")
